d55e6478da80f8bab4ae6e83b0a0f28ca27e1598
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-06 20:41:40 +0700

    Add files via upload

---
+++ faucetindonesia/config.php
@@ -0,0 +1,7 @@
+<?php
+
+//USER-AGENT
+$user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36";
+
+//COOKIE DASHBOARD
+$cookie = "_rce=VN; acceptCookies=true; dom3ic8zudi28v8lr6fgphwffqoz0j6c=fc95060b-8913-45ff-b080-b72765c067f2%3A2%3A1; sb_main_becf7aed44ebf6a8eb085ccac6e4df72=1; _ga=GA1.1.515351565.1633485391; sb_count_becf7aed44ebf6a8eb085ccac6e4df72=4; AUTH_SYSTEM=6c12064988a67e1b56b9d94451d0871d; remember_me=18c1497944701891946262867ee3d5dc69ce57762283259b76b3299cfd454011; _ga_CYJ3VEJ20G=GS1.1.1633491514.3.1.1633491623.0";
